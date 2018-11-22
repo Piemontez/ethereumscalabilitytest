cc1886ab69d5255a0adba4f77c059f6457ec5b8e
Rafael AP <piemontez@live.com>
2018-11-22 21:24:08 -0200

    contratos

--- truffle_tests/migrations/1_initial_migration.js
+++ truffle_tests/migrations/1_initial_migration.js
@@ -6,19 +6,24 @@ var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer, network, addresses) {
 
-  const config = TruffleConfig.networks[network];
-
-  if (process.env.ACCOUNT_PASSWORD) {
-    const web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
-    console.log('>> Unlocking account ' + config.from);
-    console.log("..............");
-    console.log(web3);
-    console.log("..............");
-    console.log(web3.personal);
-    console.log("..............");
-    console.log(web3.personal.unlockAccount);
-    web3.personal.unlockAccount(config.from, process.env.ACCOUNT_PASSWORD, 36000);
-  }
+//  const config = TruffleConfig.networks[network];
+//
+//  process.env.ACCOUNT_PASSWORD = "mcatestethereum";
+//  console.log("........");
+//  console.log(process.env.ACCOUNT_PASSWORD);
+//  console.log("........");
+//
+//  if (process.env.ACCOUNT_PASSWORD) {
+//    const web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
+//    console.log('>> Unlocking account ' + config.from);
+//    console.log("..............");
+//    console.log(web3);
+//    console.log("..............");
+//    console.log(web3.personal);
+//    console.log("..............");
+//    console.log(web3.personal.unlockAccount);
+//    web3.personal.unlockAccount(config.from, process.env.ACCOUNT_PASSWORD, 36000);
+//  }
 
   deployer.deploy(Migrations);
 };
--- truffle_tests/migrations/2_deploy_contracts.js
+++ truffle_tests/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const MyStringStore = artifacts.require("MyStringStore");
+const MetaCoin = artifacts.require("MetaCoin");
 
 module.exports = function(deployer) {
   deployer.deploy(MyStringStore);
+  deployer.deploy(MetaCoin);
 };
