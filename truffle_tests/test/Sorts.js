4ed6b5fd6ec43ac6d7f54e7e4c0c308b804a0c5c
Rafael AP <piemontez@live.com>
2018-11-27 12:59:47 -0200

    ajustes

--- truffle_tests/test/Sorts.js
+++ truffle_tests/test/Sorts.js
@@ -5,9 +5,15 @@ const BubbleSort = artifacts.require("BubbleSort");
 const QuickSort = artifacts.require("QuickSort");
 const MergeSort = artifacts.require("MergeSort");
 
-let results = [];
+let results = {
+  ForArray: [],
+  ForMatrix: [],
+  BubbleSort: [],
+  MergeSort: [],
+  QuickSort: [],
+};
 
-for (let x = 1; x < 2; x++) {
+for (let x = 1; x < 8; x++) {
   let ammout = Math.pow(2, x);
 
   contract("ForArray", accounts => {
@@ -15,8 +21,11 @@ for (let x = 1; x < 2; x++) {
       const contract = await ForArray.deployed();
 
       let result = await contract.execute(ammout, { from: accounts[0] });
-      console.log("Gás utilizado:" + (result.receipt||{}).gasUsed||0);
-      console.log(result);
+
+      results.ForArray.push({
+        listSize: ammout,
+        gasUsed: (result.receipt||{}).gasUsed||0
+      });
     });
   });
 
@@ -25,7 +34,12 @@ for (let x = 1; x < 2; x++) {
       const contract = await ForMatrix.deployed();
 
       let result = await contract.execute(ammout, { from: accounts[0] });
-      console.log("Gás utilizado:" + (result.receipt||{}).gasUsed||0);
+
+      results.ForMatrix.push({
+        listSize: ammout,
+        gasUsed: (result.receipt||{}).gasUsed||0
+      });
+
     });
   });
 
@@ -34,7 +48,12 @@ for (let x = 1; x < 2; x++) {
       const contract = await BubbleSort.deployed();
 
       let result = await contract.makeAndSort(ammout, { from: accounts[0] });
-      console.log("Gás utilizado:" + (result.receipt||{}).gasUsed||0);
+
+      results.BubbleSort.push({
+        listSize: ammout,
+        gasUsed: (result.receipt||{}).gasUsed||0
+      });
+
     });
   });
 
@@ -42,7 +61,12 @@ for (let x = 1; x < 2; x++) {
     it("Sort:" + ammout, async () => {
       const sort = await QuickSort.deployed();
       let result = await sort.makeAndSort(ammout, { from: accounts[0] });
-      console.log("Gás utilizado:" + (result.receipt||{}).gasUsed||0);
+
+      results.QuickSort.push({
+        listSize: ammout,
+        gasUsed: (result.receipt||{}).gasUsed||0
+      });
+
     });
   });
 
@@ -51,13 +75,22 @@ for (let x = 1; x < 2; x++) {
       const contract = await MergeSort.deployed();
 
       let result = await contract.makeAndSort(ammout, { from: accounts[0] });
-      console.log("Gás utilizado:" + (result.receipt||{}).gasUsed||0);
+
+      results.MergeSort.push({
+        listSize: ammout,
+        gasUsed: (result.receipt||{}).gasUsed||0
+      });
+
     });
   });
 }
 
 contract("Conclude results", accounts => {
   it("Making results:", async () => {
-
+    console.log("--------------------");
+    console.log("Resultado dos testes");
+    console.log("--------------------");
+    console.log(results)
+    console.log("--------------------");
   });
 });
